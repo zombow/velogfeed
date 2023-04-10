cc671cd261b3f58bf4bd86542665ad9abd0354b9
zombow <82148187+zombow@users.noreply.github.com>
2023-04-10 22:53:56 +0900

    local:3000 tested

---
+++ feed.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Parser = require("rss-parser");
+
+const app = express();
+const parser = new Parser();
+
+app.get("/", async (req, res) => {
+    const feed = await parser.parseURL("https://v2.velog.io/rss/@dksduddnr33");
+
+    const items = feed.items.map((item) => {
+        return {
+            title: item.title,
+            link: item.link,
+            date: item.pubDate,
+            description: item.content,
+        };
+    });
+
+    res.json(items);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
