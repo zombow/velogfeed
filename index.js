84c29dd5570b7d94de7b1654e9a790b0ac129981
zombow <82148187+zombow@users.noreply.github.com>
2023-04-11 15:41:32 +0900

    index edite

--- index.js
+++ index.js
@@ -11,9 +11,3 @@ app.use(function (req, res, next) {
 
 // api/feed 경로로 GET 요청이 오면 feed.js 모듈을 호출합니다.
 app.get('/api/feed', require('./api/feed'));
-
-// 서버를 구동합니다.
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
-});
