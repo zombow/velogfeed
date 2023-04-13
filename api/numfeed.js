8272435f5facb160f63e023b78a71ce3985e1bcc
zombow <82148187+zombow@users.noreply.github.com>
2023-04-13 13:03:19 +0900

    feed numrenamed numfeed

--- api/numfeed.js
+++ api/numfeed.js
@@ -9,21 +9,22 @@ module.exports = async (req, res) => {
             return res.status(400).send('username parameter is required. this is numfeed');
         }
         // 요청 파라미터에서 글순서를 가져옴
-        const feednum = req.query.numfeed;
+        let numfeed = req.query.numfeed;
         if (!numfeed) {
-            feednum = 0;
+            numfeed = 0;
+        } else {
+            numfeed = parseInt(numfeed);
         }
 
         // velog RSS 피드 URL
         const feedUrl = `https://v2.velog.io/rss/@${username}`;
         const feed = await parser.parseURL(feedUrl);
 
-
         // CORS 허용
         res.setHeader('Access-Control-Allow-Origin', '*');
 
         // JSON 형식으로 파싱된 피드를 반환
-        res.json(feed.items[feednum]);
+        res.json(feed.items[numfeed]);
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
