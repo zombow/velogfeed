84db0f9dc7ff654b9af900a44ff5e234632f439e
zombow <82148187+zombow@users.noreply.github.com>
2023-04-14 20:51:57 +0900

    add to link

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,5 +1,5 @@
-﻿const postcardSVG = (title, content) => {
-    return `
+﻿const postcardSVG = (title, link) => {
+  return `
     <svg xmlns="http://www.w3.org/2000/svg" width="400" height="180">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
@@ -17,7 +17,7 @@
       </style>
       <rect x="0" y="0" width="400" height="180" fill="#FFF"/>
       <text x="20" y="40" class="title">${title}</text>
-      <text x="20" y="80" class="content">${content}</text>
+      <text x="20" y="80" class="content">${link}</text>
     </svg>
   `;
 };
