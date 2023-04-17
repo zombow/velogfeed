734cebce3af726212cf208a57352a83a87c26800
zombow <82148187+zombow@users.noreply.github.com>
2023-04-17 22:43:55 +0900

    promise

--- api/numfeedgraphql.js
+++ api/numfeedgraphql.js
@@ -1,71 +1,63 @@
 const fetch = require('node-fetch');
 const { postcardSVG } = require('../src/card/post-card');
 
-module.exports = async (req, res) => {
-    try {
-        const username = req.query.username;
-        if (!username) {
-            return res.status(400).send('username parameter is required.');
-        }
-        let postnum = parseInt(req.query.postnum);
-        if (!postnum) {
-            postnum = 0;
-        } else {
-            postnum = parseInt(postnum);
-        }
+module.exports = (req, res) => {
+    const username = req.query.username;
+    if (!username) {
+        return res.status(400).send('username parameter is required.');
+    }
 
-        // GraphQL API 엔드포인트 URL
-        const endpointUrl = 'https://v2.velog.io/graphql';
+    let postnum = parseInt(req.query.postnum) || 0;
 
-        // GraphQL 쿼리
-        const query = `
-      query {
-        user(username: "${username}") {
-          posts(page: ${postnum / 10 + 1}, perPage: 10) {
-            edges {
-              node {
-                title
-                body
-                user {
-                  username
-                }
-                publishedAt
+    const endpointUrl = 'https://v2.velog.io/graphql';
+
+    const query = `
+    query {
+      user(username: "${username}") {
+        posts(page: ${postnum / 10 + 1}, perPage: 10) {
+          edges {
+            node {
+              title
+              body
+              user {
+                username
               }
+              publishedAt
             }
           }
         }
       }
-    `;
-
-        const response = await fetch(endpointUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ query })
-        });
-
-        console.log(response);
-
-        const result = await response.json();
-        if (!result.data) {
-            return res.status(400).send('User not found');
-        }
-
-        const post = result.data.user.posts.edges[postnum % 10].node;
-        if (!post) {
-            return res.status(400).send('Post not found');
-        }
-
-        const svg = postcardSVG(post.title, post.body);
-        const data = {
-            svg
-        };
-
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
     }
+  `;
+
+    fetch(endpointUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (!result.data) {
+                return res.status(400).send('User not found');
+            }
+
+            const post = result.data.user.posts.edges[postnum % 10]?.node;
+            if (!post) {
+                return res.status(400).send('Post not found');
+            }
+
+            const svg = postcardSVG(post.title, post.body);
+            const data = {
+                svg
+            };
+
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json(data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Server Error');
+        });
 };
