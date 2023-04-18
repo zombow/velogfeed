2513dc92e58c4589413ea1ac00e36a68880d2995
zombow <82148187+zombow@users.noreply.github.com>
2023-04-18 15:19:40 +0900

    user test

--- api/numfeedgraphql.js
+++ api/numfeedgraphql.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const { postcardSVG } = require('../src/card/post-card');
 
 module.exports = (req, res) => {
     const username = req.query.username;
@@ -7,29 +6,14 @@ module.exports = (req, res) => {
         return res.status(400).send('username parameter is required.');
     }
 
-    let postnum = parseInt(req.query.postnum) || 0;
-
     const endpointUrl = 'https://v2.velog.io/graphql';
 
     const query = `
    query {
-  user(username: "${username}") {
-    username
-    posts(page: ${postnum / 10 + 1}, perPage: 10) {
-      edges {
-        node {
-          title
-          body
-          user {
-            username
-          }
-          publishedAt
-        }
-      }
-    }
-  }
-}
-
+     user(username: "${username}") {
+       username
+     }
+   }
   `;
 
     fetch(endpointUrl, {
@@ -43,22 +27,16 @@ module.exports = (req, res) => {
     })
         .then(response => response.json())
         .then(result => {
-            console.log(result);
             if (!result.data) {
                 return res.status(400).send('result.data not found');
             }
-            const post = result.data.user.posts.edges[postnum % 10]?.node;
-            if (!post) {
-                return res.status(400).send('Post not found');
+            const user = result.data.user;
+            if (!user) {
+                return res.status(400).send('User not found');
             }
 
-            const svg = postcardSVG(post.title, post.body);
-            const data = {
-                svg
-            };
-
             res.setHeader('Access-Control-Allow-Origin', '*');
-            res.json(data);
+            res.send('Successed');
         })
         .catch(err => {
             console.error(err);
