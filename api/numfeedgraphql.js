d67e3fa3a3efd954c59456265a28f51723f67dab
zombow <82148187+zombow@users.noreply.github.com>
2023-04-18 17:12:24 +0900

    {} remove

--- api/numfeedgraphql.js
+++ api/numfeedgraphql.js
@@ -15,12 +15,11 @@ module.exports = (req, res) => {
       posts {
         title
         short_description
-          }
-        }
       }
     }
   }
 `;
+
 
 
     fetch(endpointUrl, {
@@ -34,9 +33,9 @@ module.exports = (req, res) => {
     })
         .then(response => response.json())
         .then(result => {
-           // if (!result.data) {
-           //     return res.status(400).send('result.data not found');
-           //}
+            if (!result.data) {
+                return res.status(400).send('result.data not found');
+           }
             const user = result.data.user;
             if (!user) {
                 return res.status(400).send('User not found');
