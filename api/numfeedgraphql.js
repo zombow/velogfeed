072ab854bc609e380cf9541fdf75e290e3b34cfb
zombow <82148187+zombow@users.noreply.github.com>
2023-04-19 14:05:18 +0900

    console name added

--- api/numfeedgraphql.js
+++ api/numfeedgraphql.js
@@ -2,6 +2,7 @@ const fetchPost = require("../src/fetcher/post-fetcher");
 
 module.exports = async (req, res) => {
     const { name } = req.query;
+    console.log(name);
     try {
         const post = await fetchPost(name);
         const url = new String(
