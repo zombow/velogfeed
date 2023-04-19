47a15823911cf9ee7885df44b63c5239e63ffe45
zombow <82148187+zombow@users.noreply.github.com>
2023-04-19 14:41:53 +0900

    script added

--- api/numfeedgraphql.js
+++ api/numfeedgraphql.js
@@ -1,14 +1,13 @@
 const fetchPost = require("../src/fetcher/post-fetcher");
 
 module.exports = async (req, res) => {
-    const { username } = req.query;
-    console.log(username);
+    const { name, tag } = req.query;
     try {
-        const post = await fetchPost(username);
+        const post = await fetchPost(name, tag);
         const url = new String(
             `https://velog.io/@${post.user.username}/${post.url_slug}`
         );
-        res.send(post);
+        res.send(`<script>window.location.href='${url}'</script>`);
         return;
     } catch (e) {
         return res.send(e.message);
