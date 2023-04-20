2de3eb4db11acebf61861ced4c70c09054703736
zombow <82148187+zombow@users.noreply.github.com>
2023-04-20 15:23:52 +0900

    postcard image ver.1

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -3,7 +3,7 @@
     const imageHeight = 300;
 
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="400" height="180">
+    <svg xmlns="http://www.w3.org/2000/svg" width="400" height="180" style="position: relative;">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
         .title {
@@ -17,12 +17,13 @@
           font-size: 16px;
           fill: #333;
         }
-        .thumbnauil{
-          
+        .thumbnail {
+          position: absolute;
+          top: 0;
+          left: 0;
         }
       </style>
-      <rect x="0" y="0" width="400" height="180" fill="#FFF"/>
-      <image class="thumbnail" xlink:href="${thumbnail}" x="20" y="40" width="${imageWidth}" height="${imageHeight}"/>
+      <image class="thumbnail" xlink:href="${thumbnail}" width="${imageWidth}" height="${imageHeight}"/>
       <text x="20" y="40" class="title">${title}</text>
       <text x="20" y="80" class="short_description">${short_description}</text>
     </svg>
