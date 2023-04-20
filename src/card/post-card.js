456641a0edd963312038c9f787c6f9a7b50062ca
zombow <82148187+zombow@users.noreply.github.com>
2023-04-20 19:38:14 +0900

    postcard image ver.17

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,12 +1,11 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-  const imageWidth = "100%";
-  const imageHeight = "100%";
-  const strokeWidth = 4;
-
+    const strokeWidth = 4;
     const svgWidth = 500;
-    const svgHeight = 500;
+    const svgHeight = 300;
+    const thumbnailWidth = Math.min(svgWidth, svgHeight) * 0.9;
+    const thumbnailHeight = thumbnailWidth;
 
-  return `
+    return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
@@ -21,21 +20,17 @@
           font-size: 16px;
           fill: #333;
         }
-        .thumbnail{
-          width: ${imageWidth};
-          height: ${imageHeight};
+        .thumbnail {
+          width: 100%;
+          height: 100%;
         }
       </style>
-      <g transform="translate(0,0)">
-        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0"/>
+      <g transform="translate(0, ${0.1 * svgHeight})">
+        <rect x="${strokeWidth}" y="${strokeWidth}" width="${thumbnailWidth - strokeWidth}" height="${thumbnailHeight - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+        <image class="thumbnail" xlink:href="${thumbnail}" x="${strokeWidth}" y="${strokeWidth}" width="${thumbnailWidth - strokeWidth}" height="${thumbnailHeight - strokeWidth}" />
       </g>
-      <g transform="translate(0,210)">
+      <g transform="translate(0, ${0.3 * svgHeight})">
         <text class="title" x="20" y="0">${title}</text>
         <text class="short_description" x="20" y="20">${short_description}</text>
       </g>
-      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth - strokeWidth}" height="${svgHeight - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
-    </svg>
-  `;
-};
-
-module.exports = postcardSVG;
+      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth - strokeWidth}" height="${svgHeight - strokeWidth}" stroke
