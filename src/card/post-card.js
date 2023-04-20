0eb4a0642bd6f42f1bd2c5a205a756d9b8dc5469
zombow <82148187+zombow@users.noreply.github.com>
2023-04-20 17:15:08 +0900

    postcard image ver.15

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,9 +1,9 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-    const imageWidth = "100%";
-    const imageHeight = "100%";
-    const strokeWidth = 4;
+  const imageWidth = "100%";
+  const imageHeight = "100%";
+  const strokeWidth = 4;
 
-    return `
+  return `
     <svg xmlns="http://www.w3.org/2000/svg" width="500" height="300">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
@@ -23,7 +23,6 @@
           height: ${imageHeight};
         }
       </style>
-      <rect x="${strokeWidth / 2}" y="${strokeWidth / 2}" width="${500 - strokeWidth}" height="${300 - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
       <g transform="translate(0,0)">
         <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0"/>
       </g>
@@ -31,6 +30,7 @@
         <text class="title" x="20" y="0">${title}</text>
         <text class="short_description" x="20" y="20">${short_description}</text>
       </g>
+      <rect x="${strokeWidth / 2}" y="${strokeWidth / 2}" width="${500 - strokeWidth}" height="${300 - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
     </svg>
   `;
 };
