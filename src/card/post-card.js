c2764ed6445683a037871af008a7d696b7cf5ea2
zombow <82148187+zombow@users.noreply.github.com>
2023-04-21 17:01:30 +0900

    postcard image ver.18

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,9 +1,12 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
+    const imageWidth = "100%";
+    const imageHeight = "100%";
     const strokeWidth = 4;
     const svgWidth = 500;
     const svgHeight = 300;
-    const thumbnailWidth = Math.min(svgWidth, svgHeight) * 0.9;
-    const thumbnailHeight = thumbnailWidth;
+    const thumbnailHeight = svgHeight * 0.4; // 썸네일이 차지할 높이 비율
+    const titleHeight = svgHeight * 0.2; // 타이틀이 차지할 높이 비율
+    const descriptionHeight = svgHeight * 0.2; // 설명이 차지할 높이 비율
 
     return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
@@ -20,20 +23,21 @@
           font-size: 16px;
           fill: #333;
         }
-        .thumbnail {
-          width: 100%;
-          height: 100%;
+        .thumbnail{
+          width: ${imageWidth};
+          height: ${imageHeight};
         }
       </style>
-      <g transform="translate(0, ${0.1 * svgHeight})">
-        <rect x="${strokeWidth}" y="${strokeWidth}" width="${thumbnailWidth - strokeWidth}" height="${thumbnailHeight - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
-        <image class="thumbnail" xlink:href="${thumbnail}" x="${strokeWidth}" y="${strokeWidth}" width="${thumbnailWidth - strokeWidth}" height="${thumbnailHeight - strokeWidth}" />
+      <g transform="translate(0,${(svgHeight - thumbnailHeight) / 2})">
+        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" height="${thumbnailHeight}"/>
       </g>
-      <g transform="translate(0, ${0.3 * svgHeight})">
+      <g transform="translate(0,${thumbnailHeight + (titleHeight / 2)})">
         <text class="title" x="20" y="0">${title}</text>
-        <text class="short_description" x="20" y="20">${short_description}</text>
       </g>
-      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth - strokeWidth}" height="${svgHeight - strokeWidth}" stroke ="#333" stroke-width="${strokeWidth}" fill="none" />
+      <g transform="translate(0,${thumbnailHeight + titleHeight + (descriptionHeight / 2)})">
+        <text class="short_description" x="20" y="0">${short_description}</text>
+      </g>
+      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth - strokeWidth}" height="${svgHeight - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
     </svg>
   `;
 };
