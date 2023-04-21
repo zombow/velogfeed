198f0c313db3750c5daec576c6ed246b90647297
zombow <82148187+zombow@users.noreply.github.com>
2023-04-21 17:17:52 +0900

    post card outline

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,23 +1,23 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-    const imageWidth = "100%";
-    const imageHeight = "100%";
-    const strokeWidth = 4;
-    const svgWidth = 500;
-    const svgHeight = 300;
+  const imageWidth = "100%";
+  const imageHeight = "100%";
+  const strokeWidth = 4;
+  const svgWidth = 500;
+  const svgHeight = 300;
 
-    // 썸네일 이미지의 크기와 위치를 설정합니다.
-    const thumbnailWidth = svgWidth;
-    const thumbnailHeight = svgHeight * 0.6;
-    const thumbnailX = 0;
-    const thumbnailY = 0;
+  // 썸네일 이미지의 크기와 위치를 설정합니다.
+  const thumbnailWidth = svgWidth;
+  const thumbnailHeight = svgHeight * 0.6;
+  const thumbnailX = 0;
+  const thumbnailY = 0;
 
-    // 제목과 요약 정보 영역의 크기와 위치를 설정합니다.
-    const infoWidth = svgWidth;
-    const infoHeight = svgHeight * 0.4;
-    const infoX = 0;
-    const infoY = thumbnailHeight;
+  // 제목과 요약 정보 영역의 크기와 위치를 설정합니다.
+  const infoWidth = svgWidth;
+  const infoHeight = svgHeight * 0.4;
+  const infoX = 0;
+  const infoY = thumbnailHeight;
 
-    return `
+  return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
@@ -46,7 +46,7 @@
         <text class="title" x="20" y="40">${title}</text>
         <text class="short_description" x="20" y="60">${short_description}</text>
       </g>
-      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth - strokeWidth}" height="${svgHeight - strokeWidth}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+      <rect x="${strokeWidth}" y="${strokeWidth}" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
     </svg>
   `;
 };
