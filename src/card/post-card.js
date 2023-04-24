ee2ec2edd14da0985094ac9699a4ee50e920b1fd
zombow <82148187+zombow@users.noreply.github.com>
2023-04-24 18:54:10 +0900

    thumb nail-clip test

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -15,59 +15,41 @@
     const infoX = 0;
     const infoY = thumbnailHeight;
 
-    // 이미지 크기를 구합니다.
-    const img = new Image();
-    img.src = thumbnail;
-    const imgWidth = img.width;
-    const imgHeight = img.height;
-
-    // 이미지의 가로/세로 비율을 계산합니다.
-    const imgAspectRatio = imgWidth / imgHeight;
-    const thumbnailAspectRatio = thumbnailWidth / thumbnailHeight;
-
-    // 이미지를 확대/축소하여 썸네일 영역을 가득 채우도록 계산합니다.
-    let newImgWidth, newImgHeight, cropX, cropY;
-    if (imgAspectRatio > thumbnailAspectRatio) {
-        newImgWidth = thumbnailWidth;
-        newImgHeight = newImgWidth / imgAspectRatio;
-        cropX = 0;
-        cropY = (newImgHeight - thumbnailHeight) / 2;
-    } else {
-        newImgHeight = thumbnailHeight;
-        newImgWidth = newImgHeight * imgAspectRatio;
-        cropY = 0;
-        cropX = (newImgWidth - thumbnailWidth) / 2;
+    return `
+<svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
+  <defs>
+    <clipPath id="thumbnail-clip">
+      <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}"/>
+    </clipPath>
+  </defs>
+  <style>
+    /* SVG 스타일을 여기에 추가합니다. */
+    .title {
+      font-family: Arial;
+      font-size: 24px;
+      font-weight: bold;
+      fill: #333;
     }
+    .short_description {
+      font-family: Arial;
+      font-size: 16px;
+      fill: #333;
+    }
+    .thumbnail{
+    }
+  </style>
+  <g transform="translate(${thumbnailX}, ${thumbnailY})">
+    <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
+    <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" clip-path="url(#thumbnail-clip)"/>
+  </g>
+  <g transform="translate(${infoX}, ${infoY})">
+    <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
+    <text class="title" x="20" y="40">${title}</text>
+    <text class="short_description" x="20" y="60">${short_description}</text>
+  </g>
+  <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+</svg>
 
-  return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
-      <style>
-        /* SVG 스타일을 여기에 추가합니다. */
-        .title {
-          font-family: Arial;
-          font-size: 24px;
-          font-weight: bold;
-          fill: #333;
-        }
-        .short_description {
-          font-family: Arial;
-          font-size: 16px;
-          fill: #333;
-        }
-        .thumbnail{
-        }
-      </style>
-      <g transform="translate(${thumbnailX}, ${thumbnailY})">
-        <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
-        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${cropX}" height="${cropY}"/>
-      </g>
-      <g transform="translate(${infoX}, ${infoY})">
-        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
-        <text class="title" x="20" y="40">${title}</text>
-        <text class="short_description" x="20" y="60">${short_description}</text>
-      </g>
-      <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
-    </svg>
   `;
 };
 
