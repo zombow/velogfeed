c0cc51bcf52f5ff53faafef907b8337fafe36a89
zombow <82148187+zombow@users.noreply.github.com>
2023-04-24 19:13:20 +0900

    object-fit corver test

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -16,40 +16,37 @@
     const infoY = thumbnailHeight;
 
     return `
-<svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
-  <defs>
-    <clipPath id="thumbnail-clip">
-      <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}"/>
-    </clipPath>
-  </defs>
-  <style>
-    /* SVG 스타일을 여기에 추가합니다. */
-    .title {
-      font-family: Arial;
-      font-size: 24px;
-      font-weight: bold;
-      fill: #333;
-    }
-    .short_description {
-      font-family: Arial;
-      font-size: 16px;
-      fill: #333;
-    }
-    .thumbnail{
-    }
-  </style>
-  <g transform="translate(${thumbnailX}, ${thumbnailY})">
-    <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
-    <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" clip-path="url(#thumbnail-clip)"/>
-  </g>
-  <g transform="translate(${infoX}, ${infoY})">
-    <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
-    <text class="title" x="20" y="40">${title}</text>
-    <text class="short_description" x="20" y="60">${short_description}</text>
-  </g>
-  <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
-</svg>
-
+    <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
+      <style>
+        /* SVG 스타일을 여기에 추가합니다. */
+        .title {
+          font-family: Arial;
+          font-size: 24px;
+          font-weight: bold;
+          fill: #333;
+        }
+        .short_description {
+          font-family: Arial;
+          font-size: 16px;
+          fill: #333;
+        }
+        .thumbnail{
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      </style>
+      <g transform="translate(${thumbnailX}, ${thumbnailY})">
+        <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
+        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}"/>
+      </g>
+      <g transform="translate(${infoX}, ${infoY})">
+        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
+        <text class="title" x="20" y="40">${title}</text>
+        <text class="short_description" x="20" y="60">${short_description}</text>
+      </g>
+      <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+    </svg>
   `;
 };
 
