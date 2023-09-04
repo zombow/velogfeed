7108af2b97a546e1ba397e380f9933efb422c208
zombow <82148187+zombow@users.noreply.github.com>
2023-09-04 19:16:21 +0900

    타이틀 원복

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -39,7 +39,7 @@
       </g>
       <g transform="translate(${infoX}, ${infoY})">
         <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
-        <text class="title" x="40" y="40">${title}</text>
+        <text class="title" x="20" y="40">${title}</text>
         <text class="short_description" x="20" y="60">${short_description}</text>
       </g>
       <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
