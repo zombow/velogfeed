8450c0062bd815afbb9ced4a218effb051bd307b
zombow <82148187+zombow@users.noreply.github.com>
2023-09-13 14:55:15 +0900

    numfeed-beta test1
    numfeed-beta test1

--- api/numfeed-beta.js
+++ api/numfeed-beta.js
@@ -4,23 +4,23 @@ const postcardSVG = require("../src/card/post-card");
 module.exports = async (req, res) => {
     const { username, postnum, tag } = req.query;
     try {
-        const posts = await fetchPosts({ username, postnum, tag });
-        const postcards = posts.map(post => {
-            const url = `https://velog.io/@${post.user.username}/${post.url_slug}`;
-            const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description);
-            return {
-                post: post,
-                url: url,
-                title: post.title,
-                short_description: post.short_description,
-                thumbnail: post.thumbnail,
-                svg: postSVG,
-            };
-        });
-
-        res.send(postcards); 
+        const post = await fetchPost({ username, postnum, tag });
+        const url = new String(
+            `https://velog.io/@${post.user.username}/${post.url_slug}`
+        );
+        const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description);
+        const postinfo = {
+            post: post,
+            url: url,
+            title: post.title,
+            short_description: post.short_description,
+            thumbnail: post.thumbnail,
+            svg: postSVG,
+        }
+        res.send(postinfo);
         return;
     } catch (e) {
         return res.send(e.message);
     }
 };
+;
