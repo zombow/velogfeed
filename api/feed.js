f9fd96c277c02905d7a54cf222b7e310be07fea7
zombow <82148187+zombow@users.noreply.github.com>
2023-09-13 15:11:30 +0900

    test2

--- api/feed.js
+++ api/feed.js
@@ -4,9 +4,7 @@ const postcardSVG = require("../src/card/post-card");
 module.exports = async (req, res) => {
     const { username, postnum, tag } = req.query;
     try {
-        const posts = await fetchPosts({ username, postnum, tag }); // fetchPosts 함수를 사용하여 여러 게시물 정보를 가져옵니다.
-
-        // 가져온 게시물 정보를 순회하면서 포스트카드를 생성하고 배열에 저장합니다.
+        const posts = await fetchPosts({ username, postnum, tag });
         const postcards = posts.map(post => {
             const url = `https://velog.io/@${post.user.username}/${post.url_slug}`;
             const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description);
@@ -20,8 +18,7 @@ module.exports = async (req, res) => {
             };
         });
 
-        res.send(postcards); // 배열에 저장된 포스트카드들을 응답으로 반환합니다.
-        return;
+        res.send(postcards);
     } catch (e) {
         return res.send(e.message);
     }
