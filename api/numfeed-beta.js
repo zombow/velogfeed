6fae6c32b74252dcd6693e3106f59a123503b8b2
zombow <82148187+zombow@users.noreply.github.com>
2023-09-13 15:10:05 +0900

    numfeed rollback & feed reTEST

--- api/numfeed-beta.js
+++ api/numfeed-beta.js
@@ -1,26 +1,40 @@
-const fetchPost = require("../src/fetcher/post-fetcher");
-const postcardSVG = require("../src/card/post-card");
+const Parser = require('rss-parser');
+const parser = new Parser();
+
+// postcard.js 파일에서 SVG 코드를 가져옵니다.
+const postcardSVG = require('../src/card/post-card');
 
 module.exports = async (req, res) => {
-    const { username, postnum, tag } = req.query;
     try {
-        const post = await fetchPost({ username, postnum, tag });
-        const url = new String(
-            `https://velog.io/@${post.user.username}/${post.url_slug}`
-        );
-        const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description);
-        const postinfo = {
-            post: post,
-            url: url,
-            title: post.title,
-            short_description: post.short_description,
-            thumbnail: post.thumbnail,
-            svg: postSVG,
+        // 요청 파라미터에서 사용자 이름을 가져옴
+        const username = req.query.username;
+        if (!username) {
+            return res.status(400).send('username parameter is required. this is numfeed');
         }
-        res.send(postinfo);
-        return;
-    } catch (e) {
-        return res.send(e.message);
+        // 요청 파라미터에서 글순서를 가져옴
+        let postnum = parseInt(req.query.postnum);
+        if (!postnum) {
+            postnum = 0;
+        } else {
+            postnum = parseInt(postnum);
+        }
+
+        // velog RSS 피드 URL
+        const feedUrl = `https://v2.velog.io/rss/@${username}`;
+        const feed = await parser.parseURL(feedUrl);
+
+        // CORS 허용
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // JSON 형식으로 파싱된 피드를 반환
+        const post = feed.items[postnum];
+        const svg = postcardSVG(post.title, post.contentSnippet);
+        const data = {
+            svg: svg
+        }
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
     }
 };
-;
