988dfdd7871082491790abecae7b58cefb0a8524
zombow <82148187+zombow@users.noreply.github.com>
2023-09-15 19:03:08 +0900

    post card test

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,7 +1,7 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
     const strokeWidth = 2;
-    const svgWidth = 800;
-    const svgHeight = 450;
+    const svgWidth = 700;
+    const svgHeight = 350;
 
     // 썸네일 이미지의 크기와 위치를 설정합니다.
     const thumbnailWidth = svgWidth;
