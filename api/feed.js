e5833f40f553b30dd16907e10a6f040930ca60ab
zombow <82148187+zombow@users.noreply.github.com>
2023-09-15 19:37:44 +0900

    test3

--- api/feed.js
+++ api/feed.js
@@ -13,8 +13,8 @@ module.exports = async (req, res) => {
 
         for (const post of filteredPosts) {
             const url = `https://velog.io/@${post.user.username}/${post.url_slug}`;
-            const maxTitleLength = 30; // 제목 최대 길이
-            const maxDescriptionLength = 38; // 요약 정보 최대 길이
+            const maxTitleLength = 28; // 제목 최대 길이
+            const maxDescriptionLength = 40; // 요약 정보 최대 길이
 
             const title = post.title.length > maxTitleLength ? post.title.slice(0, maxTitleLength) + '...' : post.title;
             const shortDescription = post.short_description.length > maxDescriptionLength ? post.short_description.slice(0, maxDescriptionLength) + '...' : post.short_description;
