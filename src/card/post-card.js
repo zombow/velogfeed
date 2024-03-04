2b3ff2a774a13dbdec4b7cb4d12b4b3ba2877dc7
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 23:32:38 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -2,6 +2,7 @@
     const strokeWidth = 2;
     const svgWidth = 700;
     const svgHeight = 400;
+    const cornerRadius = 15; // 테두리의 라운드 설정
 
     // 썸네일 이미지의 크기와 위치를 설정합니다.
     const thumbnailWidth = svgWidth;
@@ -33,16 +34,19 @@
         .thumbnail{
         }
       </style>
+      <!-- 테두리 그리기 -->
+      <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
       <g transform="translate(${thumbnailX}, ${thumbnailY})">
+        <!-- 썸네일 이미지 -->
         <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
         <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
       </g>
       <g transform="translate(${infoX}, ${infoY})">
+        <!-- 제목과 요약 정보 -->
         <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
         <text class="title" x="20" y="40">${title}</text>
         <text class="short_description" x="30" y="80">${short_description}</text>
       </g>
-      <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
     </svg>
   `;
 };
