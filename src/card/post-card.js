e136cd7f71eb17dbb3c034f47f047fbba9e1cd2a
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 23:22:15 +0900

    테두리 색상변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -2,8 +2,7 @@
     const strokeWidth = 2;
     const svgWidth = 700;
     const svgHeight = 400;
-    const cornerRadiusTop = 15; // 상단 두 모서리의 라운드 설정
-    const cornerRadiusBottom = 15; // 하단 두 모서리의 라운드 설정
+    const cornerRadius = 15; // 상단 두 모서리의 라운드 설정
 
     // 썸네일 이미지의 크기와 위치를 설정합니다.
     const thumbnailWidth = svgWidth;
@@ -36,7 +35,7 @@
         }
       </style>
       <!-- 테두리 그리기 -->
-      <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="rgba(51, 51, 51, 0.5)" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadiusTop}" ry="${cornerRadiusBottom}" />
+      <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
       <g transform="translate(${thumbnailX}, ${thumbnailY})">
         <!-- 썸네일 이미지 -->
         <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" /> 
