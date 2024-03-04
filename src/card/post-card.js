2ee3a779a4800c0bfd22358a76d18a7913ed3beb
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 21:13:42 +0900

    라운드 조정

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -3,7 +3,7 @@
     const svgWidth = 700;
     const svgHeight = 400;
     const cornerRadiusTop = 40; // 상단 두 모서리의 라운드 설정
-    const cornerRadiusBottom = 0; // 하단 두 모서리의 라운드 설정
+    const cornerRadiusBottom = 20; // 하단 두 모서리의 라운드 설정
 
     // 썸네일 이미지의 크기와 위치를 설정합니다.
     const thumbnailWidth = svgWidth;
