8fedfb825340de283d2045e056d14f4c23ac061e
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 21:06:22 +0900

    이미지제거 테스트

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -39,7 +39,7 @@
       <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadiusTop}" ry="${cornerRadiusTop}" />
       <g transform="translate(${thumbnailX}, ${thumbnailY})">
         <!-- 썸네일 이미지 -->
-        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
+       <!-- <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />  -->
       </g>
       <g transform="translate(${infoX}, ${infoY})">
         <!-- 제목 -->
