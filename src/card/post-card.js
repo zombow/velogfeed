a238182c2a3161b778c293c75fd2c851b60b8214
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 19:51:56 +0900

    Top, Botton Rounded Edidte

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -35,12 +35,14 @@
         }
       </style>
       <g transform="translate(${thumbnailX}, ${thumbnailY})">
-        <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
+        <!-- 썸네일의 상단을 라운드 형태로 만듭니다. -->
+        <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" rx="${cornerRadius}" ry="${cornerRadius}" fill="#ccc"/>
         <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
       </g>
       <g transform="translate(${infoX}, ${infoY})">
-        <!-- 카드를 그립니다. -->
-        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" rx="${cornerRadius}" ry="${cornerRadius}" stroke="#333" stroke-width="${strokeWidth}" fill="#eee" />
+        <!-- ShortDescription의 하단을 라운드 형태로 만듭니다. -->
+        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="#eee" />
+        <rect x="0" y="${infoHeight - cornerRadius}" width="${infoWidth}" height="${cornerRadius}" fill="#eee" rx="${cornerRadius}" ry="${cornerRadius}" />
         <text class="title" x="20" y="40">${title}</text>
         <text class="short_description" x="30" y="80">${short_description}</text>
       </g>
