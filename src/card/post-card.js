cd6d2b4335cca06a1734e855d744a5eab29b60db
zombow <82148187+zombow@users.noreply.github.com>
2024-03-05 00:22:31 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -39,14 +39,12 @@
         <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc" rx="${cornerRadius}" ry="${cornerRadius}" />
         <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
       </g>
-      <g transform="translate(${infoX}, ${infoY})" rx="${cornerRadius}" ry="${cornerRadius}">
-        <!-- 제목과 요약 정보 -->
-        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee" />
-        <text class="title" x="20" y="40">${title}</text>
-        <text class="short_description" x="30" y="80">${short_description}</text>
-      </g>
+      <!-- 제목과 요약 정보 -->
+      <rect x="${infoX}" y="${infoY}" width="${infoWidth}" height="${infoHeight}" fill="#eee" rx="${cornerRadius}" ry="${cornerRadius}" />
+      <text class="title" x="${infoX + 20}" y="${infoY + 40}">${title}</text>
+      <text class="short_description" x="${infoX + 30}" y="${infoY + 80}">${short_description}</text>
       <!-- 테두리 그리기 -->
-      <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+      <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
     </svg>
   `;
 };
