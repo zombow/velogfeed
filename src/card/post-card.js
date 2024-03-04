9f116249a3a30cfd483d9db71112d5604afe352c
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 19:23:27 +0900

    SVG Image Edite 2

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -16,36 +16,39 @@
     const infoY = thumbnailHeight;
 
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="100">
-        <rect width="200" height="100" fill="#f0f0f0" stroke="#333" stroke-width="2" rx="10" ry="10"/>
-      <style>
-        /* SVG 스타일을 여기에 추가합니다. */
-        .title {
-          font-family: Arial;
-          font-size: 28px;
-          font-weight: bold;
-          fill: #333;
-        }
-        .short_description {
-          font-family: Arial;
-          font-size: 22px;
-          fill: #333;
-        }
-        .thumbnail{
-        }
-      </style>
-      <g transform="translate(${thumbnailX}, ${thumbnailY})">
-        <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
-        <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
-      </g>
-      <g transform="translate(${infoX}, ${infoY})">
-        <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee"/>
-        <text class="title" x="20" y="40">${title}</text>
-        <text class="short_description" x="30" y="80">${short_description}</text>
-      </g>
-      <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
-    </svg>
-  `;
+<svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
+  <style>
+    /* SVG 스타일을 여기에 추가합니다. */
+    .title {
+      font-family: Arial;
+      font-size: 28px;
+      font-weight: bold;
+      fill: #333;
+    }
+    .short_description {
+      font-family: Arial;
+      font-size: 22px;
+      fill: #333;
+    }
+    .thumbnail{
+    }
+  </style>
+  <g transform="translate(${thumbnailX}, ${thumbnailY})">
+    <rect x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" fill="#ccc"/>
+    <image class="thumbnail" xlink:href="${thumbnail}" x="0" y="0" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
+  </g>
+  <g transform="translate(${infoX}, ${infoY})">
+    <!-- 제목 텍스트를 감싸는 사각형 요소 -->
+    <rect x="0" y="0" width="${infoWidth}" height="${infoHeight}" fill="#eee" rx="10" ry="10"/>
+    <text class="title" x="20" y="40">${title}</text>
+    <!-- 요약 텍스트를 감싸는 사각형 요소 -->
+    <rect x="0" y="50" width="${infoWidth}" height="${infoHeight - 50}" fill="#eee" rx="10" ry="10"/>
+    <text class="short_description" x="20" y="90">${short_description}</text>
+  </g>
+  <rect x= 0 y= 0 width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" />
+</svg>
+`;
+
 };
 
 module.exports = postcardSVG;
