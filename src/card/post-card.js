ea370e5b2e18c11e78a527e11b89280826aadbaa
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 21:04:57 +0900

    stroke 테두리 값변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,5 +1,5 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-    const strokeWidth = 2;
+    const strokeWidth = 1;
     const svgWidth = 700;
     const svgHeight = 400;
     const cornerRadiusTop = 20; // 상단 두 모서리의 라운드 설정
