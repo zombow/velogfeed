59103de16eb23d43fa0f38b9ca63496d01a14c55
zombow <82148187+zombow@users.noreply.github.com>
2024-03-04 18:49:53 +0900

    Text font Test

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -21,7 +21,7 @@
         /* SVG 스타일을 여기에 추가합니다. */
         .title {
           font-family: Arial;
-          font-size: 28px;
+          font-size: 52px;
           font-weight: bold;
           fill: #333;
         }
