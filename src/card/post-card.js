e4f9efc53308b63edc3ad995b25732f8b938fffa
zombow <82148187+zombow@users.noreply.github.com>
2024-03-05 23:09:06 +0900

    글자수제한 2차

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,5 +1,7 @@
-﻿const postcardSVG = (title, thumbnail, short_description, maxCharacters) => {
-    // 텍스트가 최대 길이를 초과하면 일정 길이까지만 자르고 ... 추가
+﻿const postcardSVG = (title, thumbnail, short_description) => {
+    const maxCharacters = 20; // 최대 글자 수 설정
+
+    // 텍스트가 최대 글자 수를 초과하면 일정 길이까지만 자르고 ... 추가
     const shortenText = (text, maxLength) => {
         if (text.length > maxLength) {
             return text.substring(0, maxLength) + '...';
@@ -16,10 +18,10 @@
         <image xlink:href="${thumbnail}" width="100%" height="65%" x="0" y="0" preserveAspectRatio="xMidYMid slice" />
 
         <!-- 제목을 줄이고 생략 (...) 처리 -->
-        <text x="5%" y="20%" font-family="Arial" font-size="20" font-weight="bold" fill="#333" text-anchor="start">${shortenedTitle}</text>
+        <text x="5%" y="75%" font-family="Arial" font-size="20" font-weight="bold" fill="#333" text-anchor="start">${shortenedTitle}</text>
 
         <!-- 설명을 줄이고 생략 (...) 처리 -->
-        <text x="5%" y="30%" font-family="Arial" font-size="16" fill="#333" text-anchor="start">${shortenedDescription}</text>
+        <text x="5%" y="85%" font-family="Arial" font-size="16" fill="#333" text-anchor="start">${shortenedDescription}</text>
 
         <rect width="100%" height="100%" stroke="#333" stroke-width="2" fill="none" rx="15" ry="15" />
     </svg>
