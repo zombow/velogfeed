45bff34efd928cca1d37fc445c28725e31e7c2ba
zombow <82148187+zombow@users.noreply.github.com>
2024-03-05 15:03:50 +0900

    하이트 값조절

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,6 +1,6 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" style="width: 20vw; height: auto; max-width: 700px;"> <!-- SVG의 뷰박스와 크기 설정 -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" style="width: 20vw; height: 200px; max-width: 700px;"> <!-- SVG의 뷰박스와 크기 설정 -->
         <rect width="100%" height="100%" fill="#eee" rx="15" ry="15" />
         <image xlink:href="${thumbnail}" width="100%" height="65%" x="0" y="0" preserveAspectRatio="xMidYMid slice" />
         <text x="50%" y="70%" font-family="Arial" font-size="2vw" font-weight="bold" fill="#333" text-anchor="middle">${title}</text>
