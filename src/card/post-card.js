1a66b78dba5f7876c0bec76a4ad6fca3b7178665
zombow <82148187+zombow@users.noreply.github.com>
2024-03-05 13:58:51 +0900

    리펙터링 2차

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,38 +1,27 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-    const strokeWidth = 2;
-    const svgWidth = "100%";
-    const svgHeight = "100%";
-    const cornerRadius = 15; // 모서리의 라운드 설정
+    const svgWidth = "80vw"; // 카드의 너비를 화면 너비의 80%로 설정
+    const svgHeight = "80vh"; // 카드의 높이를 화면 높이의 80%로 설정
+    const cornerRadius = "15px"; // 모서리의 라운드 설정
 
-    // 제목과 요약 정보 텍스트의 위치 설정
+    // 썸네일 이미지의 크기와 위치를 설정합니다.
+    const thumbnailWidth = "100%";
+    const thumbnailHeight = "65%";
+    const thumbnailX = "0";
+    const thumbnailY = "0";
+
+    // 제목과 요약 정보 영역의 위치를 설정합니다.
     const titleX = "5%";
-    const titleY = "10%";
+    const titleY = "75%";
     const descriptionX = "5%";
-    const descriptionY = "20%";
+    const descriptionY = "85%";
 
     return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
-      <style>
-        /* SVG 스타일을 여기에 추가합니다. */
-        .title {
-          font-family: Arial;
-          font-size: 4vw; /* 텍스트 크기를 화면 크기에 상대적으로 설정합니다. */
-          font-weight: bold;
-          fill: #333;
-        }
-        .short_description {
-          font-family: Arial;
-          font-size: 3vw; /* 텍스트 크기를 화면 크기에 상대적으로 설정합니다. */
-          fill: #333;
-        }
-        .thumbnail{
-        }
-      </style>
-      <rect x="0" y="0" width="100%" height="100%" fill="#eee" rx="${cornerRadius}" ry="${cornerRadius}" />
-      <image class="thumbnail" xlink:href="${thumbnail}" width="100%" height="65%" preserveAspectRatio="xMidYMid slice" />
-      <text class="title" x="${titleX}" y="${titleY}">${title}</text>
-      <text class="short_description" x="${descriptionX}" y="${descriptionY}">${short_description}</text>
-      <rect x="0" y="0" width="100%" height="100%" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
+      <rect width="100%" height="100%" fill="#eee" rx="${cornerRadius}" ry="${cornerRadius}" />
+      <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" x="${thumbnailX}" y="${thumbnailY}" preserveAspectRatio="xMidYMid slice" />
+      <text x="${titleX}" y="${titleY}" font-family="Arial" font-size="4vw" font-weight="bold" fill="#333">${title}</text>
+      <text x="${descriptionX}" y="${descriptionY}" font-family="Arial" font-size="3vw" fill="#333">${short_description}</text>
+      <rect width="100%" height="100%" stroke="#333" stroke-width="2" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
     </svg>
   `;
 };
