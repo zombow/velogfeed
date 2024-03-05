d897bac24cecf168268cbb7a1561bd75cda63690
zombow <82148187+zombow@users.noreply.github.com>
2024-03-05 13:42:05 +0900

    뷰박스 사용

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,40 +1,39 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
     const strokeWidth = 2;
-    const svgWidth = "700 * 100%";
-    const svgHeight = "100%";
-    const cornerRadius = 15;
+    const svgWidth = 700;
+    const svgHeight = 400;
+    const cornerRadius = 15; // 모서리의 라운드 설정
 
     // 썸네일 이미지의 크기와 위치를 설정합니다.
-    const thumbnailWidth = "100%";
-    const thumbnailHeight = "65%";
+    const thumbnailWidth = svgWidth;
+    const thumbnailHeight = svgHeight * 0.65;
     const thumbnailX = 0;
     const thumbnailY = 0;
 
     // 제목과 요약 정보 영역의 크기와 위치를 설정합니다.
-    const infoWidth = "100%";
-    const infoHeight = "35%";
+    const infoWidth = svgWidth;
+    const infoHeight = svgHeight * 0.35;
     const infoX = 0;
-    const infoY = "65%";
+    const infoY = thumbnailHeight;
 
-    // 제목과 요약 정보의 텍스트 위치를 설정합니다.
-    const titleX = "5%";
-    const titleY = "10%";
-    const descriptionX = "5%";
-    const descriptionY = "20%";
+    // viewBox 계산
+    const viewBoxWidth = svgWidth + strokeWidth * 2;
+    const viewBoxHeight = svgHeight + strokeWidth * 2;
+    const viewBox = `0 0 ${viewBoxWidth} ${viewBoxHeight}`;
 
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
+    <svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}" viewBox="${viewBox}">
       <style>
         /* SVG 스타일을 여기에 추가합니다. */
         .title {
           font-family: Arial;
-          font-size: 4vh; /* 텍스트 크기를 화면 크기에 상대적으로 설정합니다. */
+          font-size: 28px;
           font-weight: bold;
           fill: #333;
         }
         .short_description {
           font-family: Arial;
-          font-size: 3vh; /* 텍스트 크기를 화면 크기에 상대적으로 설정합니다. */
+          font-size: 22px;
           fill: #333;
         }
         .thumbnail{
@@ -48,8 +47,8 @@
       <!-- 제목과 요약 정보 -->
       <rect x="${infoX}" y="${infoY}" width="${infoWidth}" height="${infoHeight}" fill="#eee" />
       <rect x="${infoX}" y="${infoY + infoHeight - cornerRadius}" width="${infoWidth}" height="${cornerRadius}" fill="#eee" rx="${cornerRadius}" ry="${cornerRadius}" />
-      <text class="title" x="${titleX}" y="${titleY}">${title}</text>
-      <text class="short_description" x="${descriptionX}" y="${descriptionY}">${short_description}</text>
+      <text class="title" x="${infoX + 20}" y="${infoY + 40}">${title}</text>
+      <text class="short_description" x="${infoX + 30}" y="${infoY + 80}">${short_description}</text>
       <!-- 테두리 그리기 -->
       <rect x="0" y="0" width="${svgWidth}" height="${svgHeight}" stroke="#333" stroke-width="${strokeWidth}" fill="none" rx="${cornerRadius}" ry="${cornerRadius}" />
     </svg>
