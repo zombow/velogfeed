c09516417f6aa4055a257d3e69ba2ccaced35db9
zombow <82148187+zombow@users.noreply.github.com>
2024-03-06 15:42:39 +0900

    글자 패딩추가

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,4 +1,8 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
+    // 패딩 값 설정
+    const paddingLeft = 10;
+    const paddingTop = 10;
+
     return `
     <svg xmlns="http://www.w3.org/2000/svg" width="430" height="160" viewBox="0 0 430 160" fill="none">
         <style>
@@ -17,14 +21,16 @@
         <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
         <g data-testid="card-title" transform="translate(25, 35)">
             <g transform="translate(0, 0)">
-                <text class="header" data-testid="header">${title}</text>
+                <!-- 패딩 적용 -->
+                <text class="header" data-testid="header" x="${paddingLeft}" y="${paddingTop}">${title}</text>
             </g>
         </g>
         <g data-testid="main-card-body" transform="translate(0, 45)">
             <svg x="25" width="400" height="400" viewBox="0 0 400 400">
                 <g transform="translate(0, 0)">
-                    <text class="list-style" x="5" y="20">•</text>
-                    <text class="log-title" x="20" y="20">${short_description}</text>
+                    <!-- 패딩 적용 -->
+                    <text class="list-style" x="${paddingLeft}" y="${paddingTop}">•</text>
+                    <text class="log-title" x="${paddingLeft + 15}" y="${paddingTop + 15}">${short_description}</text>
                 </g>
             </svg>
         </g>
