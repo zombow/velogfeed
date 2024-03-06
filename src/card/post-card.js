370bf843355bcb959954941f2eb9b38bd63b46dc
zombow <82148187+zombow@users.noreply.github.com>
2024-03-06 16:35:47 +0900

    rect 크기변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -17,7 +17,7 @@
             .log-title:hover{ fill: #0CA678; text-decoration: underline;}
             .list-style{font-size:14px; fill: #212529; }
         </style>
-        <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
+        <rect x="0.5" y="0.5" rx="4.5" height="700" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
         <!-- 썸네일 이미지 추가 -->
         <image xlink:href="${thumbnail}" width="100%" height="65%" x="${padding}" y="${padding}" />
         <g data-testid="card-title" transform="translate(${padding}, ${padding})">
