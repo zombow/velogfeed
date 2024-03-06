fee45dfa4ad51c3bb0976426ada2f7b12e23955d
zombow <82148187+zombow@users.noreply.github.com>
2024-03-06 16:45:33 +0900

    썸네일 패딩제거

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -21,7 +21,7 @@
         <g data-testid="card-title" transform="translate(${padding}, ${padding})">
             <g transform="translate(0, 0)">
                 <!-- 썸네일 이미지 추가 -->
-                <image xlink:href="${thumbnail}" width="100%" height="65%" x="${padding}" y="${padding}" />
+                <image xlink:href="${thumbnail}" width="100%" height="65%" />
                 <!-- 타이틀에 패딩 적용 -->
                 <text class="header" x="${padding}" y="${padding + 90}" data-testid="header">${title}</text>
             </g>
