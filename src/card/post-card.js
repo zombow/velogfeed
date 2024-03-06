a7f6971d707f2ee4c855f1095f21d44a4e5f277d
zombow <82148187+zombow@users.noreply.github.com>
2024-03-06 15:44:24 +0900

    패딩추가 2

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -1,7 +1,9 @@
 ﻿const postcardSVG = (title, thumbnail, short_description) => {
-    // 패딩 값 설정
-    const paddingLeft = 10;
-    const paddingTop = 10;
+    // 타이틀과 쇼트 디스크립션에 대한 패딩 값 설정
+    const titlePaddingLeft = 10;
+    const titlePaddingTop = 10;
+    const descriptionPaddingLeft = 10;
+    const descriptionPaddingTop = 30; // 타이틀과의 간격을 조절하기 위한 값
 
     return `
     <svg xmlns="http://www.w3.org/2000/svg" width="430" height="160" viewBox="0 0 430 160" fill="none">
@@ -21,16 +23,15 @@
         <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
         <g data-testid="card-title" transform="translate(25, 35)">
             <g transform="translate(0, 0)">
-                <!-- 패딩 적용 -->
-                <text class="header" data-testid="header" x="${paddingLeft}" y="${paddingTop}">${title}</text>
+                <!-- 타이틀에 패딩 적용 -->
+                <text class="header" data-testid="header" x="${titlePaddingLeft}" y="${titlePaddingTop}">${title}</text>
             </g>
         </g>
         <g data-testid="main-card-body" transform="translate(0, 45)">
             <svg x="25" width="400" height="400" viewBox="0 0 400 400">
                 <g transform="translate(0, 0)">
-                    <!-- 패딩 적용 -->
-                    <text class="list-style" x="${paddingLeft}" y="${paddingTop}">•</text>
-                    <text class="log-title" x="${paddingLeft + 15}" y="${paddingTop + 15}">${short_description}</text>
+                    <!-- 쇼트 디스크립션에 패딩 적용 -->
+                    <text class="list-style" x="${descriptionPaddingLeft}" y="${descriptionPaddingTop}">${short_description}</text>
                 </g>
             </svg>
         </g>
