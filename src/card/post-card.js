3fb585d89b99e1fed19e6bcd030bcfc47c173e5f
zombow <82148187+zombow@users.noreply.github.com>
2024-03-07 15:51:59 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -13,8 +13,8 @@
                 fill: #343A40;
                 animation: fadeInAnimation 0.8s ease-in-out forwards;
             }
-            .log-title { font: bold 14px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
-            .log-description { font-size: 12px; fill: #495057}
+            .log-title { font: bold 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
+            .log-description { font-size: 14px; fill: #495057}
             .tag-item { font-size: 12px; fill: #0CA678;}
             .heart-count { font-size: 12px; fill: #495057;}
             .log-title:hover{ fill: #0CA678; text-decoration: underline;}
@@ -26,10 +26,10 @@
         <g data-testid="card-title" transform="translate(${padding}, ${padding})">
             <g transform="translate(0, 0)">
                 <!-- 타이틀에 패딩 적용 -->
-                <text class="header" x="${padding}" y="${padding + 200}" data-testid="header">${title}</text>
+                <text class="header" x="${padding}" y="${padding + 210}" data-testid="header">${title}</text>
             </g>
         </g>
-        <g data-testid="main-card-body" transform="translate(${padding}, ${padding + 220})">
+        <g data-testid="main-card-body" transform="translate(${padding}, ${padding + 225})">
             <svg x="0" width="400" height="400" viewBox="0 0 400 400">
                 <g transform="translate(0, 0)">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
