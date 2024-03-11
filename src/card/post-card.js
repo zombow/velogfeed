8dda5a2a775f166860be0b9e7f770e4708366c8e
zombow <82148187+zombow@users.noreply.github.com>
2024-03-11 16:38:47 +0900

    width 값 함수화

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -23,11 +23,11 @@
         <defs>
             <clipPath id="clip-path">
                 <!-- 이미지의 상단 부분에만 라운드 적용 -->
-                <rect x="0.5" y="0.5" rx="4.5" width="429" height="170" />
+                <rect x="0.5" y="0.5" rx="4.5" width="${thumbnailWidth}" height="170" />
             </clipPath>
             <clipPath id="clip-path-bottom">
                 <!-- 이미지의 하단 부분에만 라운드 적용하지 않음 -->
-                <rect x="0.5" y="${thumbnailHeight - 170}" width="429" height="170" />
+                <rect x="0.5" y="${thumbnailHeight - 170}" width="${thumbnailWidth}" height="170" />
             </clipPath>
         </defs>
         <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
