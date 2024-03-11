e45df8a8eab4bbb3cf673e4107d924318eb2a985
zombow <82148187+zombow@users.noreply.github.com>
2024-03-11 16:00:33 +0900

    rx test

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -20,7 +20,7 @@
             .log-title:hover{ fill: #0CA678; text-decoration: underline;}
             .list-style{font-size:14px; fill: #212529; }
         </style>
-        <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
+        <rect x="0.5" y="0.5" rx="14.5" height="99%" stroke="#e4e2e2" width="429" fill="#fffefe" stroke-opacity="1"/>
         <!-- 썸네일 이미지 추가 -->
         <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" />
         <g data-testid="card-title" transform="translate(${padding}, ${padding + 140})">
