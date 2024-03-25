6e7292bb37a2fb3f948456810125dc5b90c7a92d
zombow <82148187+zombow@users.noreply.github.com>
2024-03-26 01:16:45 +0900

    썸네일이미지 전체 라운딩

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -23,14 +23,12 @@
         <defs>
             <clipPath id="clip-path">
                 <!-- 이미지의 상단 부분에만 라운드 적용 -->
-                <rect x="1" y="1" rx="4" width="98.8%"" height="${thumbnailHeight}" />
+                <rect x="1" y="1" rx="4" width="99%"" height="${thumbnailHeight}" />
             </clipPath>
         </defs>
         <rect x="0.5" y="0.5" rx="4.5" height="99%" width="99%" stroke="#e4e2e2" fill="#fffefe" stroke-opacity="1"/>
         <!-- 썸네일 이미지 추가 -->
         <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path)" />
-        <!-- 하단 부분에만 라운드 적용하지 않은 이미지 -->
-        <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path-bottom)" />
         <g data-testid="card-title" transform="translate(${padding}, ${padding + 140})">
             <svg width="400" height="400" viewBox="0 0 400 300">
                 <g transform="translate(0, 0)">
