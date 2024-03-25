24ec0c64035b7ee42466a06e973c7daf075a797a
zombow <82148187+zombow@users.noreply.github.com>
2024-03-25 18:41:38 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -30,14 +30,15 @@
                 <rect x="1" y="${thumbnailHeight / 2}" width="100%" height="${thumbnailHeight}" />
             </clipPath>
         </defs>
+        <!-- 외각선을 먼저 그리는 것이 아니라 이미지를 먼저 추가 -->
         <g>
-            <!-- 외각선을 먼저 그림 -->
-            <rect x="0.5" y="0.5" rx="4.5" height="99%" width="99%" stroke="red" fill="#fffefe" stroke-opacity="1"/>
             <!-- 썸네일 이미지 추가 -->
             <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path)" />
             <!-- 하단 부분에만 라운드 적용하지 않은 이미지 -->
             <image xlink:href="${thumbnail}" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path-bottom)" />
         </g>
+        <!-- 외각선을 이미지 위에 그림 -->
+        <rect x="0.5" y="0.5" rx="4.5" height="99%" width="99%" stroke="red" fill="#fffefe" stroke-opacity="1"/>
         <g data-testid="card-title" transform="translate(${padding}, ${padding + 140})">
             <svg width="400" height="400" viewBox="0 0 400 300">
                 <g transform="translate(0, 0)">
