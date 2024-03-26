2d06156e084d1e204728d30d3df60234c45f36e2
zombow <82148187+zombow@users.noreply.github.com>
2024-03-26 19:09:48 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -2,15 +2,15 @@
     // 패딩 값 설정
     const padding = 10;
     // 이미지의 고정된 너비와 높이 설정
-    const thumbnailWidth = 417;
-    const thumbnailHeight = 187;
+    const thumbnailWidth = 420;
+    const thumbnailHeight = 190;
 
     // 썸네일 이미지를 원하는 위치로 이동시키기 위한 좌표 설정
     const thumbnailX = 5; // 원하는 x 좌표
     const thumbnailY = 5; // 원하는 y 좌표
 
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="430" height="300" fill="fffefe">
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="fffefe">
         <style>
             .header {
                 font: bold 14px 'Segoe UI', Ubuntu, Sans-Serif;
