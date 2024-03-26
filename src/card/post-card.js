def07b1e57ab93524bacdcfa44b9c94c416fca63
zombow <82148187+zombow@users.noreply.github.com>
2024-03-26 20:49:08 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -26,7 +26,7 @@
     </style>
     <defs>
         <clipPath id="clip-path">
-            <rect rx="8" />
+            <rect x="15" y="15 "rx="8" width=${thumbnailWidth}  height=${thumbnailHeight} />
         </clipPath>
         <!-- 그림자 효과를 위한 필터 정의 -->
         <filter id="drop-shadow" x="-20%" y="-20%" width="130%" height="130%">
