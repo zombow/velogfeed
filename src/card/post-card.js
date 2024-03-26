e405094eb1e7ac774d756a393df8dbd61c2b6731
zombow <82148187+zombow@users.noreply.github.com>
2024-03-26 19:43:22 +0900

    스트로크 두께 변경후 테스트

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -29,7 +29,7 @@
                 <rect rx="8" width="${thumbnailWidth}"  height="${thumbnailHeight}" />
             </clipPath>
         </defs>
-        <rect x="0.5" y="0.5" rx="5" width="429" height="299" stroke="#e4e2e2" fill="#fffefe" stroke-opacity="1"/>
+        <rect x="1" y="1" rx="5" width="498" height="298" stroke="#e4e2e2" fill="#fffefe" stroke-opacity="2"/>
         <!-- 썸네일 이미지 추가 -->
         <image xlink:href="${thumbnail}" transform="translate(${thumbnailX}, ${thumbnailY})" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path)" />
         <g data-testid="card-title" transform="translate(${(430 - (430 - (padding*2))) / 2}, 215)">
