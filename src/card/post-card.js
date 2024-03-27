b5909ac44918af30ed98d37175ec39632bd995db
zombow <82148187+zombow@users.noreply.github.com>
2024-03-27 12:51:05 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -6,8 +6,8 @@
     const thumbnailHeight = 190;
 
     // 썸네일 이미지를 원하는 위치로 이동시키기 위한 좌표 설정
-    const thumbnailX = 7.5; // 원하는 x 좌표
-    const thumbnailY = 7.5; // 원하는 y 좌표
+    const thumbnailX = 10; // 원하는 x 좌표
+    const thumbnailY = 10; // 원하는 y 좌표
 
     return `
 <svg xmlns="http://www.w3.org/2000/svg" width="440px" height="310px" fill="fffefe">
