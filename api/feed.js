3e8da55bf6d2bd8813781acd4294f09239c59574
zombow <82148187+zombow@users.noreply.github.com>
2024-03-30 02:45:47 +0900

    tags 수정

--- api/feed.js
+++ api/feed.js
@@ -2,9 +2,9 @@ const fetchPosts = require("../src/fetcher/post-fetcher");
 const postcardSVG = require("../src/card/post-card");
 
 module.exports = async (req, res) => {
-    const { username, postnum} = req.query;
+    const { username, postnum, tag } = req.query;
     try {
-        const posts = await fetchPosts({ username, postnum});
+        const posts = await fetchPosts({ username, postnum, tag });
         const postinfoList = [];
 
         // postnum ���� ���� ���͸��Ͽ� ����Ʈ ������ �����ɴϴ�.
@@ -13,7 +13,7 @@ module.exports = async (req, res) => {
 
         for (const post of filteredPosts) {
             const url = `https://velog.io/@${post.user.username}/${post.url_slug}`;
-            const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description, post.tag);
+            const postSVG = postcardSVG(post.title, post.thumbnail, post.short_description, post.tags);
             
             const postinfo = {
                 post: post,
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
                 title: post.title,
                 short_description: post.short_description,
                 thumbnail: post.thumbnail,
-                tag: post.tag,
+                tags: post.tags,
                 svg: postSVG,
             };
             postinfoList.push(postinfo);
--- src/fetcher/post-fetcher.js
+++ src/fetcher/post-fetcher.js
@@ -3,13 +3,12 @@ const { request } = require("../util");
 const fetcher = (variables) => {
     return request({
         query: `
-            query Posts($cursor: ID, $username: String, $temp_only: Boolean, $postnum: Int) {
-                posts(cursor: $cursor, username: $username, temp_only: $temp_only, limit: $postnum) {
+            query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $postnum: Int) {
+                posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $postnum) {
                   id
                   title
                   short_description
                   thumbnail
-                  tags
                   user {
                     username
                     profile {
@@ -20,6 +19,7 @@ const fetcher = (variables) => {
                   released_at
                   updated_at
                   comments_count
+                  tags
                   likes
                 }
               }
