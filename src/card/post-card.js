659d337e37757fe24e2fb0451ae1a676e2409ad7
zombow <82148187+zombow@users.noreply.github.com>
2024-04-01 16:28:25 +0900

    tag 배경색, 글자색상 변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -27,8 +27,8 @@
 
         tagsGroup.push(`
             <g data-testid="tag-group-${index}" transform="translate(${tagX}, ${tagGroupY})">
-                <rect x="0" y="0" width="${tagWidth}" height="${tagHeight}" rx="10" fill="#e9ecef" />
-                <text x="${tagWidth / 2}" y="${tagHeight / 2}" dominant-baseline="middle" text-anchor="middle" fill="#495057">${tag}</text>
+                <rect x="0" y="0" width="${tagWidth}" height="${tagHeight}" rx="10" fill="#f8f9fa" />
+                <text class="tag-item" x="${tagWidth / 2}" y="${tagHeight / 2}" dominant-baseline="middle" text-anchor="middle" fill="#495057">${tag}</text>
             </g>
         `);
     });
@@ -39,7 +39,7 @@
                 .header { font: bold 14px 'Segoe UI', Ubuntu, Sans-Serif; fill: #343A40; animation: fadeInAnimation 0.8s ease-in-out forwards; }
                 .log-title { font: bold 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
                 .log-description { font-size: 16px; fill: #495057}
-                .tag-item { font-size: 12px; fill: #0CA678;}
+                .tag-item { font-size: 12px; fill: #12B886;}
                 .heart-count { font-size: 12px; fill: #495057;}
                 .log-title:hover{ fill: #0CA678; text-decoration: underline;}
                 .list-style{ font-size:14px; fill: #212529; }
