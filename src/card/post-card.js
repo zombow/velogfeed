4e76c82ff8f48afa71fc9d5bfe6667755e061da6
zombow <82148187+zombow@users.noreply.github.com>
2024-04-01 17:47:43 +0900

    font 추가변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -36,7 +36,7 @@
     return `
         <svg xmlns="http://www.w3.org/2000/svg" width="440px" height="310px" fill="fffefe">
             <style>
-                .header { font: bold 14px Georgia, Sans-Serif; fill: #343A40; animation: fadeInAnimation 0.8s ease-in-out forwards; }
+                .header { font: bold 14px Tahoma, Sans-Serif; fill: #343A40; animation: fadeInAnimation 0.8s ease-in-out forwards; }
                 .log-title { font: bold 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
                 .log-description { font-size: 16px; fill: #495057}
                 .tag-item { font-size: 14px; fill: #12B886;}
