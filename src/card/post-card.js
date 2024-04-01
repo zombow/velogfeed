4d6959909f380dfad6371a1d1254a3af39cde6e0
zombow <82148187+zombow@users.noreply.github.com>
2024-04-01 20:56:29 +0900

    타이틀, 숏디스크립션 위치변경

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -67,19 +67,19 @@
             <rect x="${thumbnailX}" y="${thumbnailY}" rx="8"  width="${thumbnailWidth}" height="${thumbnailHeight}" stroke="#c6c9ce" stroke-width="1.5" fill="none" />
             <!-- 썸네일 이미지 추가 -->
             <image xlink:href="${thumbnail}" transform="translate(${thumbnailX} , ${thumbnailY})" width="${thumbnailWidth}" height="${thumbnailHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-path)" />
-            <g data-testid="card-username" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 185)">
+            <g data-testid="card-username" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 180)">
                 <svg width=${430 - (padding * 2)} height="300">
                     <!-- 유저네임 패딩 적용 -->
                     <text class="header" x="0" y="35" data-testid="header">${user.username + ".Log"}</text>
                 </svg>
             </g>
-            <g data-testid="card-title" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 210)">
+            <g data-testid="card-title" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 212)">
                 <svg width=${430 - (padding * 2)} height="300">
                     <!-- 타이틀에 패딩 적용 -->
                     <text class="log-title" x="0" y="35" data-testid="log-title">${title}</text>
                 </svg>
             </g>
-            <g data-testid="card-body" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 235)">
+            <g data-testid="card-body" transform="translate(${(430 - (430 - (padding * 2))) / 2}, 232)">
                 <svg width=${430 - (padding * 2)} height="300">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
                     <text class="log-description" x="0" y="35">${short_description}</text>
