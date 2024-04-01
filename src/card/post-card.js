df7d30ef04c49285310779c334940b23aec93092
zombow <82148187+zombow@users.noreply.github.com>
2024-04-01 14:25:03 +0900

    태그 길이 테스트

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -13,12 +13,12 @@
     const tagSpacing = 5;
 
     // 태그를 담을 그룹 요소의 시작 y 좌표
-    let tagGroupY = 210;
+    let tagGroupY = 205;
     let tagGroupX = padding;
     // 태그를 담을 그룹 요소 생성
     const tagsGroup = tags.map((tag, index) => {
         // 태그의 길이에 따라 동적으로 너비 계산
-        const tagWidth = Math.min(tag.length * 8, 150); // 최대 너비는 150이며, 글자 수에 따라 동적으로 변경됨
+        const tagWidth = tag.length * 10; // 글자 수에 따라 동적으로 변경됨
         const tagHeight = 20;
         const tagX = index * (tagWidth + tagSpacing);
 
