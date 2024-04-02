c0621a0e73b27150dc6df0a20170cc88a123181a
zombow <82148187+zombow@users.noreply.github.com>
2024-04-02 21:03:01 +0900

    '' 감싸기 제거후 , 사용

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -38,12 +38,12 @@
             <style>
                 @font-face {
                 font-family: 'Warhaven';
-                src: url('../fonts/WarhavenBold.ttf') format('truetype');
-                     url('../fonts/WarhavenRegular.ttf') format('truetype');
-                     url('../fonts/WarhavenOTFBold.otf') format('opentype');
+                src: url('../fonts/WarhavenBold.ttf') format('truetype'),
+                     url('../fonts/WarhavenRegular.ttf') format('truetype'),
+                     url('../fonts/WarhavenOTFBold.otf') format('opentype'),
                      url('../fonts/WarhavenOTFRegular.otf') format('opentype');
                 }
-                .header { font: bold 14px 'Warhaven', Sans-Serif; fill: #343A40; animation: fadeInAnimation 0.8s ease-in-out forwards; }
+                .header { font: bold 14px Warhaven, Sans-Serif; fill: #343A40; animation: fadeInAnimation 0.8s ease-in-out forwards; }
                 .log-title { font: bold 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
                 .log-description { font-size: 16px; fill: #495057}
                 .tag-item { font-size: 14px; fill: #12B886;}
