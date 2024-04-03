80da9167f9f91cba4dd63d1dbdc302ae93f0465e
zombow <82148187+zombow@users.noreply.github.com>
2024-04-03 21:53:41 +0900

    변경 테스트

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -39,8 +39,9 @@
             <style>
                 @font-face {
                     font-family:'WarhavenB';
-                    src: url('../fonts/Warhaven_Bold.ttf') format("woff"),
-                    url('../fonts/WarhavenOTF_Bold.otf') format("woff");
+                    src: url('../fonts/Warhaven_Bold.ttf');
+                    src: url('../fonts/Warhaven_Bold.ttf') format('truetype'), 
+                        url('../fonts/WarhavenOTF_Bold.otf') format('opentype');
                 }                   
                 .header { font: bold 15px 'WarhavenB', Sans-Serif; fill: #343A40;}
                 .log-title { font: bold 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #212529 }
