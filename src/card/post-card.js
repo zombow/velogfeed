138baa56e4156c0ef04855e8a998df85db283e8c
zombow <82148187+zombow@users.noreply.github.com>
2024-05-13 16:25:17 +0900

    ellipsis 만적용

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -61,6 +61,12 @@
                 .heart-count { font-size: 12px; fill: #495057;}
                 .log-title:hover{ fill: #12B886; text-decoration: underline;}
                 .list-style{ font-size:14px; fill: #212529; }
+                .ellipsis{
+                display:block;
+                white-space:nowrap;
+                overflow:hidden;
+                text-overflow:ellipsis;
+                }
               
                 /* 호버 애니메이션 */
                 .post-card-container {
@@ -114,7 +120,7 @@
             <g data-testid="card-body" transform="translate(${(cardbodyX - (cardbodyX - (padding * 2))) / 2}, 227)">
                 <svg width=${cardbodyX - (padding * 2)} height="${cardbodyY}">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
-                    <text class="log-description" x="0" y="35">${short_description}</text>
+                    <text class="ellipsis" x="0" y="35">${short_description}</text>
                 </svg>
             </g>
             <!-- 태그 추가 -->
