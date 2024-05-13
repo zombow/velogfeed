ac0ba576638fee4ea3cb4ebcaca8edbe981b94ea
zombow <82148187+zombow@users.noreply.github.com>
2024-05-13 20:25:00 +0900

    div 삭제 테스트

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -113,9 +113,7 @@
             <g data-testid="card-body" transform="translate(${(cardbodyX - (cardbodyX - (padding * 2))) / 2}, 227)">
                 <svg width=${cardbodyX - (padding * 2)} height="${cardbodyY}">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
-                     <div class="ellipsis">
-                        <text class="log-description ellipsis" x="0" y="35">${short_description}</text>
-                    </div>
+                    <text class="log-description ellipsis" x="0" y="35">${short_description}</text>
                 </svg>
             </g>
             <!-- 태그 추가 -->
