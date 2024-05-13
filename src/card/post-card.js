ed5fcdc7eda516a25fc77414eec79ae76d7937e2
zombow <82148187+zombow@users.noreply.github.com>
2024-05-13 13:08:13 +0900

    text ellipsis 적용

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -54,7 +54,9 @@
                 .heart-count { font-size: 12px; fill: #495057;}
                 .log-title:hover{ fill: #12B886; text-decoration: underline;}
                 .list-style{ font-size:14px; fill: #212529; }
-              
+                .textellipsis{
+                text-overflow-ellipsis: elipsis;
+                }
                 /* 호버 애니메이션 */
                 .post-card-container {
                     transition: transform 0.3s ease-out; /* 호버와 호버 해제에 대한 트랜지션 설정 */
@@ -107,7 +109,7 @@
             <g data-testid="card-body" transform="translate(${(cardbodyX - (cardbodyX - (padding * 2))) / 2}, 227)">
                 <svg width=${cardbodyX - (padding * 2)} height="${cardbodyY}">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
-                    <text class="log-description" x="0" y="35" textLength="1200" lengthAdjust="spacingAndGlyphs" >${short_description}</text>
+                    <text class="log-description textellipsis" x="0" y="35" lengthAdjust="spacingAndGlyphs" >${short_description}</text>
                 </svg>
             </g>
             <!-- 태그 추가 -->
