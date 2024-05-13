1fb981bd4a76d2856ee960b7714ba1b58bebf93b
zombow <82148187+zombow@users.noreply.github.com>
2024-05-13 15:26:56 +0900

    Update post-card.js

--- src/card/post-card.js
+++ src/card/post-card.js
@@ -44,7 +44,7 @@
     });
 
     return `
-    <div class="post-card-container">
+    <div class="post-card-container ellipsis">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${postcardX} ${postcardY}" width="100%" preserveAspectRatio="xMidYMid slice">
             <style>               
                 .header { font: bold 15px 'Warhaven', Sans-Serif; fill: #343A40;}
@@ -113,7 +113,7 @@
             <g data-testid="card-body" transform="translate(${(cardbodyX - (cardbodyX - (padding * 2))) / 2}, 227)">
                 <svg width=${cardbodyX - (padding * 2)} height="${cardbodyY}">
                     <!-- 쇼트 디스크립션에 패딩 적용 -->
-                    <text class="log-description ellipsis" x="0" y="35">${short_description}</text>
+                    <text class="log-description" x="0" y="35">${short_description}</text>
                 </svg>
             </g>
             <!-- 태그 추가 -->
